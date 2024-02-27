e5d3004f1f0d53569e72120b87a2258d2e197321
Nathan Shower <nathan@kanopistudios.com>
2024-02-27 11:08:10 -0500

    add banner, fix null issue

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Banner({result, answer, numGuesses}) {
+
+  const bannerText = 'win' === result
+    ? <p><strong>Congratulations!</strong> You got it in {numGuesses} guesses!</p>
+    : <p>Sorry, the correct answer was <strong>{answer}</strong>.</p>;
+
+  return (
+    <div className="banner">
+      {bannerText}
+    </div>
+  );
+}
+
+export default Banner;
--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -5,12 +5,13 @@ import { range } from '../../utils';
 function Guess({ guess, guessResult }) {
 
   return (
-    <li className="guess" key={guess.id}>
+    <li className="guess" data-key={guess.id} key={guess.id}>
       {
         range(5).map( (col) => (
           <span
             key={col}
-            className={`cell ${guessResult && guessResult[col].status}`}
+            data-key={col}
+            className={`cell ${guessResult ? guessResult[col].status : ''}`}
           >
             {guess.word.substring(col, col + 1)}
           </span>
